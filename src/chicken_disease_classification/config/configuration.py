429f2a23946aa0f9eeda7462935f4ab842bc3e63
Dharmit Patel <dharmitpatel81@gmail.com>
2025-05-22 14:46:27 -0700

    feat(stage-5): Added model evaluation metrics and integrate DVC for reproducibility

--- src/chicken_disease_classification/config/configuration.py
+++ src/chicken_disease_classification/config/configuration.py
@@ -4,8 +4,10 @@ from chicken_disease_classification.entity.config_entity import (
     DataIngestionConfig,
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
-    TrainingConfig)
+    TrainingConfig,
+    EvaluationConfig)
 import os
+from pathlib import Path
 
 
 class ConfigurationManager:
@@ -87,3 +89,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.keras"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
